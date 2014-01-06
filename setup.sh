9e76f83a339ff4f2ed061476d323941215eca94e
Moiz Merchant <moiz@electronicdreams>
2014-01-05 20:59:56 -0500

    Setup checks for pre existing dot files.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,65 @@
-#!/usr/env bash
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# setup
+#------------------------------------------------------------------------------
 
 # grab the script directory
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Installing dotfiles from ${DOTFILES}..."
+
+#------------------------------------------------------------------------------
+# functions
+#------------------------------------------------------------------------------
+
+install_file_link()
+{
+    if ([ -h $1 ] || [ ! -f $1 ]) then
+        ln -fs ${DOTFILES}/$1 ~/$1
+    else
+        echo "  - skipping $1 already exists..."
+    fi
+}
+
+#------------------------------------------------------------------------------
+
+install_dir_link()
+{
+    if ([ -h ~/$1 ] || [ ! -d ~/$1 ]) then
+        ln -fhs ${DOTFILES}/$1 ~/$1
+    else
+        echo "  - skipping $1 already exists..."
+    fi
+}
+
+#------------------------------------------------------------------------------
+# install
+#------------------------------------------------------------------------------
 
 #
 # setup aliases
 #
-ln -s ${DOTFILES}/.aliases ~/.aliases
+echo "Adding aliases..."
+install_file_link ".aliases"
 
 #
 # setup vim
 #
-ln -s ${DOTFILES}/.vimrc ~/.vimrc
-ln -s ${DOTFILES}/.vim ~/.vim
-mkdir ~/.vimswap
+echo "Configuring vim..."
+install_file_link ".vimrc"
+install_dir_link ".vim"
+if ([ ! -d ~/.vimswap ]) then
+    mkdir ~/.vimswap
+fi
 
 #
 # setup git
 #
-ln -s ${DOTFILES}/.gitconfig ~/.gitconfig
+echo "Configuring git..."
+install_file_link ".gitconfig"
 
 #
 # setup tmux
 #
-ln -s ${DOTFILES}/.tmux.conf ~/.tmux.conf
+echo "Configuring tmux..."
+install_file_link ".tmux.conf"
